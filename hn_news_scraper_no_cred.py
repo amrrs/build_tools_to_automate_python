f368efc4017dd15d90498f4ee4226c718f6fb0b6
amrrs <amrrs@outlook.com>
2019-04-25 20:30:04 +0530

    new code - HN top

---
+++ hn_news_scraper_no_cred.py
@@ -0,0 +1,79 @@
+import requests
+from bs4 import BeautifulSoup
+# Send the mail
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import datetime
+now = datetime.datetime.now()
+
+content = ''
+
+
+#extracting Hacker News Stories
+
+
+def extract_news(url):
+    print('Extracting Hacker News Stories...')
+    cnt = ''
+    cnt +=('<b>HN Top Stories:</b>\n'+'<br>'+'-'*50+'<br>')
+    response = requests.get(url)
+    content = response.content
+    soup = BeautifulSoup(content,'html.parser')
+    for i,tag in enumerate(soup.find_all('td',attrs={'class':'title','valign':''})):
+        cnt += ((str(i+1)+' :: '+tag.text + "\n" + '<br>') if tag.text!='More' else '')
+        #print(tag.prettify) #find_all('span',attrs={'class':'sitestr'}))
+    return(cnt)
+    
+cnt = extract_news('https://news.ycombinator.com/')
+content += cnt
+content += ('<br>------<br>')
+content +=('<br><br>End of Message')
+
+
+#lets send the email
+
+print('Composing Email...')
+
+# update your email details
+
+SERVER = 'smtp.gmail.com' # "your smtp server"
+PORT = 587 # your port number
+FROM =  '' # "your from email id"
+TO = '' # "your to email ids"  # can be a list
+PASS = '' # "your email id's password"
+
+# fp = open(file_name, 'rb')
+# Create a text/plain message
+# msg = MIMEText('')
+msg = MIMEMultipart()
+
+# msg.add_header('Content-Disposition', 'attachment', filename='empty.txt')
+msg['Subject'] = 'Top News Stories HN [Automated Email]' + ' ' + str(now.day) + '-' + str(now.month) + '-' + str(
+    now.year)
+msg['From'] = FROM
+msg['To'] = TO
+
+msg.attach(MIMEText(content, 'html'))
+# fp.close()
+
+print('Initiating Server...')
+
+server = smtplib.SMTP(SERVER, PORT)
+#server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+server.set_debuglevel(1)
+server.ehlo()
+server.starttls()
+#server.ehlo
+server.login(FROM, PASS)
+server.sendmail(FROM, TO, msg.as_string())
+
+print('Email Sent...')
+
+server.quit()
+
+
+
+
+
+
